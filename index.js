9d8074d97802777ace66fa40e8940bdb236068d8
Vaibhavshakya25 <vaibhavshakyabharthana@gmail.com>
2023-05-07 20:55:58 +0530

    proxy

--- index.js
+++ index.js
@@ -11,10 +11,16 @@ const PORT = process.env.PORT || 6000;
 
 // Link The Router File
 app.use(require('./router/auth'));
-app.use(cors({
-    origin: "*"
+app.use(cors());
+const { createProxyMiddleware } = require('http-proxy-middleware');
+app.use('/api', createProxyMiddleware({ 
+    target: 'http://localhost:3000/', //original url
+    changeOrigin: true, 
+    //secure: false,
+    onProxyRes: function (proxyRes, req, res) {
+       proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+    }
 }));
-
 app.listen(PORT,()=>{
     console.log('Server is Started ' + PORT);
 })
