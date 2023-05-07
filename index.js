5ed3c65e957228ba0924737552d4a83cd838eb45
Vaibhavshakya25 <vaibhavshakyabharthana@gmail.com>
2023-05-07 20:13:08 +0530

    up

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 6000;
 // Link The Router File
 app.use(require('./router/auth'));
 app.use(cors({
-    origin: "https://vaibhavshakya.netlify.app"
+    origin: "*"
 }));
 
 app.listen(PORT,()=>{
