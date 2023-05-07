2a91901c7e247baba3d9973cde1e7adada78d1c0
Vaibhavshakya25 <vaibhavshakyabharthana@gmail.com>
2023-05-07 18:02:15 +0530

    Update corse

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ const PORT = process.env.PORT || 6000;
 
 // Link The Router File
 app.use(require('./router/auth'));
-app.use(cors());
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions));
 
 app.listen(PORT,()=>{
     console.log('Server is Started ' + PORT);
