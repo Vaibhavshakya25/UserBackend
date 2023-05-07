d44077f16233b11337513f8e3a212f2fc201eb43
Vaibhavshakya25 <vaibhavshakyabharthana@gmail.com>
2023-05-07 18:52:18 +0530

    Update corse error

--- index.js
+++ index.js
@@ -11,13 +11,9 @@ const PORT = process.env.PORT || 6000;
 
 // Link The Router File
 app.use(require('./router/auth'));
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions));
+app.use(cors({
+    origin: "https://backfirst.onrender.com/"
+}));
 
 app.listen(PORT,()=>{
     console.log('Server is Started ' + PORT);
